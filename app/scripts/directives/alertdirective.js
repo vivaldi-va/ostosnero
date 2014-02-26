d37f084df1b62d3c0cc839c715ce351fa3066ab2
zac <zaccary.price@avansera.com>
2014-02-26 13:38:08 +0200

    Fix alert bar not appearing or disappearing properly

--- app/scripts/directives/alertdirective.js
+++ app/scripts/directives/alertdirective.js
@@ -3,27 +3,39 @@
  */
 
 angular.module('App.Directives')
-	.directive('onAlert', function($timeout, $animate) {
+	.directive('onAlert', function($timeout, $log) {
 
 		return {
 			restrict: 'EA',
 			replace: true,
-			template: '<div class="alert {{ error.type }}">{{ error.message }}</div>',
+			template: '<div class="alert {{ error.type }}" ng-if="visible" ng-animate="\'alert\'">{{ error.message }}</div>',
 			scope: {
 				error: '='
 			},
 			link: function(scope, elem, attrs) {
 
-				scope.visible = scope.error.hasOwnProperty('message');
+				scope.visible = false;//scope.error.hasOwnProperty('message');
+				$log.debug('onAlert', "alert should show?", scope.visible, scope.error);
+				scope.$watch('error', function(newValue, oldValue) {
 
-				if(scope.visible) {
-					$animate.enter(elem.parent());
-					if(scope.error.type !== 'error') {
-	                 	$timeout(function(){
-							$animate.leave(elem);
-						}, 2000);
+					$log.debug('onAlert', "scope watcher for scope.error", oldValue, newValue);
+
+					if(!!newValue) {
+						scope.visible = true;
+						if(scope.error.type !== 'error') {
+							$timeout(function(){
+								//$animate.leave(elem);
+								scope.visible = false;
+							}, 2000);
+						}
 					}
-				}
+				});
+
+
+				/*if(scope.visible) {
+					$animate.enter(elem.parent());
+
+				}*/
 			}
 		}
 	});
